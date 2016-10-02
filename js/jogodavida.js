dcf0ca076f89a4dab017e440bb9a61d893fcb3eb
Cris <cristinamotinham@gmail.com>
2016-10-02 00:21:04 -0300

    Making code more reusable and more useful tests

--- js/jogodavida.js
+++ js/jogodavida.js
@@ -1,119 +1,134 @@
-var celula = [[],[],[]]; 
-for (var i = 0; i<3; i++) {
-	celula[i] = [[],[],[]]; //cria a matriz 3x3
+var celula = [];
+var numLinhas = 3;
+var numColunas = 3;
+
+for (var i = 0; i < numLinhas; i++) {
+    celula[i] = new Array(numColunas); //gerador de matriz
 }
-
-function contarVizinhos(i,j) {
-
-	var numVizinhos = 0;
-	var maxLinha = 2;
-	var maxColuna = 2;
-
-	 // vizinho a esquerda
-    if (j - 1 >= 0){
-      numVizinhos += celula[i][j-1];
-    }
-    // vizinho a direita
-    if (j + 1 <= maxColuna){
-      numVizinhos += celula[i][j+1];
-    }
-    
-    // vizinho acima
-    if (i - 1 >= 0){
-      numVizinhos += celula[i-1][j];
-    }
-    
-    // vizinho abaixo
-    if (i + 1 <= maxLinha){
-      numVizinhos += celula[i+1][j];
-    }
-    
-    // vizinho cima - esquerda
-    if ( (j - 1 >= 0) && (i - 1 >= 0) ){
-      numVizinhos += celula[i-1][j-1];
-    }
-
-    // vizinho cima - direita
-    if ( (i - 1 >= 0) && (j + 1 <= maxColuna) ){
-      numVizinhos += celula[i-1][j+1];
-    }
-
-    // vizinho baixo - esquerda
-    if ( (i + 1 <= maxLinha) && (j - 1 >= 0) ){
-      numVizinhos += celula[i+1][j-1];
-    }
-    
-    // vizinho baixo - direita
-    if ( (i + 1 <= maxLinha) && (j + 1 <= maxColuna) ){
-      numVizinhos += celula[i+1][j+1];
-    }
-	
-	return numVizinhos;
-}
-
-
-function checarVida (i,j) { 
-
-	celulaTeste = celula[i][j];
-    vizinhos = contarVizinhos(i,j);
-
-    if (celulaTeste == 1 && vizinhos < 2) {
-    	document.getElementById("resultado").innerHTML = "Morta por baixa populacao";
-    	return 0;
-    }
-    else if (celulaTeste == 1 && vizinhos > 3) { 
-    	document.getElementById("resultado").innerHTML = "Morta por alta populacao";
-    	return 0;
-    }
-    else if (celulaTeste == 1 && (vizinhos == 2 || vizinhos == 3)) {
-    	document.getElementById("resultado").innerHTML = "A celula continua viva";
-    	return 1;
-    }
-    else if (celulaTeste == 0 && vizinhos == 3) {
-    	document.getElementById("resultado").innerHTML = "A celula revive";
-    	return 1;
-    }
-    else {
-    	document.getElementById("resultado").innerHTML = "A celula continua morta";
-        return 0;
-    }
-
-};
 
 function pegarElementos(){
 
     formSelectHTML = document.getElementById('select');
-    matrizHTML = document.getElementById('matriz');
-    console.log("carreguei")  
+    matrizHTML = document.getElementById('matriz'); 
+}
+
+var valoresCelula = [];
+
+function salvarMatriz(matriz){ 
+    for (var i = 0; i < (matriz.length - 1); i++) {
+        if (matriz.elements[i].value == 1){
+            valoresCelula.push(1)
+        } else {
+            matriz.elements[i].value = 0;
+            valoresCelula.push(0)
+        }
+    }
+    criarMatrizCelular(valoresCelula);
+}
+
+function criarMatrizCelular(valoresCelula) {
+    for(var i = 0; i < celula.length; i++) {
+        for(var j = 0; j < celula[i].length; j++) {
+            celula[i][j] = valoresCelula[i*celula.length+j];
+        }
+    }   
 }
 
 function selecionarCelula(formSelect){
-    for (var i = 0; i<formSelect.length; i++) {
+    for (var i = 0; i < formSelect.length - 1; i++) {
         if (formSelect.elements[i].checked) {;
             x = (formSelect.elements[i].id).substring(1,2);
             y = (formSelect.elements[i].id).substring(2,3);
             x = parseInt(x);
             y = parseInt(y);
             checarVida(x,y);
+            retornarEstadoCelula(resultado);
         }
     }
-    
 }
 
-function salvarMatriz(matriz){ 
-    for (var i = 0; i<(matriz.length -1); i++) {
-        x = (matriz.elements[i].id).substring(0,1);
-        y = (matriz.elements[i].id).substring(1,2);
-        x = parseInt(x);
-        y = parseInt(y);
-        if (matriz.elements[i].value == 1){
-            celula[x][y] = 1;
-        } else {
-            matriz.elements[i].value = 0;
-            celula[x][y] = 0;
-        }
+function contarVizinhos(eixoX,eixoY) {
+
+	var numVizinhos = 0;
+	var maxLinha = 2;
+	var maxColuna = 2;
+
+	 // vizinho a esquerda
+    if (eixoY - 1 >= 0){
+      numVizinhos += celula[eixoX][eixoY-1];
     }
-    return 1;
+    // vizinho a direita
+    if (eixoY + 1 <= maxColuna){
+      numVizinhos += celula[eixoX][eixoY+1];
+    }
+    
+    // vizinho acima
+    if (eixoX - 1 >= 0){
+      numVizinhos += celula[eixoX-1][eixoY];
+    }
+    
+    // vizinho abaixo
+    if (eixoX + 1 <= maxLinha){
+      numVizinhos += celula[eixoX+1][eixoY];
+    }
+    
+    // vizinho cima - esquerda
+    if ( (eixoY - 1 >= 0) && (eixoX - 1 >= 0) ){
+      numVizinhos += celula[eixoX-1][eixoY-1];
+    }
+
+    // vizinho cima - direita
+    if ( (eixoX - 1 >= 0) && (eixoY + 1 <= maxColuna) ){
+      numVizinhos += celula[eixoX-1][eixoY+1];
+    }
+
+    // vizinho baixo - esquerda
+    if ( (eixoX + 1 <= maxLinha) && (eixoY - 1 >= 0) ){
+      numVizinhos += celula[eixoX+1][eixoY-1];
+    }
+    
+    // vizinho baixo - direita
+    if ( (eixoX + 1 <= maxLinha) && (eixoY + 1 <= maxColuna) ){
+      numVizinhos += celula[eixoX+1][eixoY+1];
+    }
+	
+	return numVizinhos;
 }
+
+var resultado;
+
+function checarVida (eixoX,eixoY) { 
+
+	celulaTeste = celula[eixoX][eixoY];
+    vizinhos = contarVizinhos(eixoX,eixoY);
+
+    if (celulaTeste == 1 && vizinhos < 2) {
+    	resultado = "Morta por baixa populacao";
+    	return 0;
+    }
+    if (celulaTeste == 1 && vizinhos > 3) { 
+    	resultado = "Morta por alta populacao";
+    	return 0;
+    }
+    if (celulaTeste == 1 && (vizinhos == 2 || vizinhos == 3)) {
+    	resultado = "A celula continua viva";
+    	return 1;
+    }
+    if (celulaTeste == 0 && vizinhos == 3) {
+    	resultado = "A celula revive";
+    	return 1;
+    }
+    resultado = "A celula continua morta";
+    return 0;
+    
+};
+
+function retornarEstadoCelula(resultado){
+    document.getElementById("resultado").innerHTML = resultado;
+}
+
+
+
+
 
 
--- test/jogodavidaSpec.js
+++ test/jogodavidaSpec.js
@@ -1,52 +1,15 @@
 var expect = chai.expect;
 
-describe('salvarMatriz', function(){
-    it('transforma pedaço da id em indices de celula', function() {
-        matriz = document.createElement('form');
-        matriz.setAttribute("id", "matriz");
-        matriz.innerHTML = 
-        '<h3>Insira sua matriz:</h3>\
-        <table>\
-            <tr>\
-                <td><input type="number" id="00" min="0" max="1" value="1"></td>\
-                <td><input type="number" id="01" min="0" max="1"></td>\
-                <td><input type="number" id="02" min="0" max="1"></td>\
-            </tr>\
-            <tr>\
-                <td><input type="number" id="10" min="0" max="1"></td>\
-                <td><input type="number" id="11" min="0" max="1" value="1"></td>\
-                <td><input type="number" id="12" min="0" max="1"></td>\
-            </tr>\
-            <tr>\
-                <td><input type="number" id="20" min="0" max="1" value="1"></td>\
-                <td><input type="number" id="21" min="0" max="1"></td>\
-                <td><input type="number" id="22" min="0" max="1" value="1"></td>\
-            </tr>\
-        </table>\
-        <input type="button" value="Salvar Matriz" onclick="salvarMatriz(matrizHTML)"> '
-
-        sinon.stub(document, 'getElementById');
-        document.getElementById.withArgs('matriz').returns(matriz);
-        salvarMatriz(matriz);
-        expect(x).to.be.a('number');
-        expect(y).to.be.a('number');
-    })
-});
-
 describe('contarVizinhos', function(){
     it('conta vizinhos em todas as direções', function() {
-        salvarMatriz(matriz);
+        var valoresCelulaTeste = [1, 0, 0, 0, 1, 0, 1, 0, 1];
+        criarMatrizCelular(valoresCelulaTeste);
         contagemTeste = contarVizinhos(1,1);
         expect(contagemTeste).to.equal(3)
     })
 })
 
 describe('checarVida', function(){
-    beforeEach(function(){
-        div = document.createElement('div');
-        div.setAttribute("id", "resultado");
-        document.getElementById.withArgs('resultado').returns(div);
-    })
     it('verifica se continua viva', function() {
         checarVida(1,1);
         expect(celulaTeste).to.equal(1);
